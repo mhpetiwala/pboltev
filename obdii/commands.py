0b31b3d8b41a3c39cf35c5c9a12aae764968220f
Mo Petiwala <46502867+mhpetiwala@users.noreply.github.com>
2023-07-19 17:34:02 -0500

    Update commands.py

--- obdii/commands.py
+++ obdii/commands.py
@@ -17,11 +17,13 @@ ext_commands = {
     'CAN_RECEIVE_ADDRESS_7EC': OBDCommand("CAN_RECEIVE_ADDRESS_7EC", "Set the CAN receive address to 7EC"          , b"ATCRA7EC",  0, raw_string          , ECU.UNKNOWN, False),
     'CAN_RECEIVE_ADDRESS_7EF': OBDCommand("CAN_RECEIVE_ADDRESS_7EF", "Set the CAN receive address to 7EF"          , b"ATCRA7EF",  0, raw_string          , ECU.UNKNOWN, False),
 
-    'CAN_FILTER_7CE':          OBDCommand("CAN_FILTER_7CE",          "Set the CAN filter to 7CE"                           , b"ATCF7CE" ,  0, raw_string          , ECU.UNKNOWN, False),
-    
-    'BAT_SOC':                 OBDCommand("BAT_SOC",                "Battery State Of Charge"          , b"228334"    ,  0, bat_soc, ECU.ALL    , False),  
-    'AMBIENT_AIR_TEMP':                 OBDCommand("AMBIENT_AIR_TEMP",                "Ambient Air Temp"          , b"220046"    ,  0, coolant_temp, ECU.ALL    , False),  
-    'BAT_COOLANT_TEMP':                 OBDCommand("BAT_COOLANT_TEMP",                "Battery Coolant Temp"          , b"2241A4"    ,  0, coolant_temp, ECU.ALL    , False),  
+    'BAT_PACK_CAP_AH_RAW_2018':       OBDCommand("BAT_PACK_CAP_AH_RAW_2018",          "Battery Capacity Ah Raw 2017 - 2018"            , b"2241a3"     ,  0, bat_ah_raw_2018, ECU.ALL    , False),  
+    'BAT_PACK_CAP_AH_RAW_2019':       OBDCommand("BAT_PACK_CAP_AH_RAW_2019",          "Battery Capacity Ah Raw 2019+"                  , b"2245f9"     ,  0, bat_ah_raw_2019, ECU.ALL    , False),  
+    'BAT_PACK_CAP_AH_EST_2018':       OBDCommand("BAT_PACK_CAP_KWH_EST_2018",          "Battery Capacity KWh Est 2017 - 2018"          , b"2241a3"     ,  0, bat_ah_raw_2018, ECU.ALL    , False),  
+    'BAT_PACK_CAP_AH_EST_2019':       OBDCommand("BAT_PACK_CAP_KWH_EST_2019",          "Battery Capacity KWh Est 2019+"                , b"2245f9"     ,  0, bat_ah_raw_2019, ECU.ALL    , False),  
+    'BAT_PACK_SOC_DISP':       OBDCommand("BAT_PACK_SOC_DISP",          "Battery SoC Displayed"          , b"228334"    ,  0, bat_soc, ECU.ALL    , False),  
+    'AMBIENT_AIR_TEMP':                  OBDCommand("AMBIENT_AIR_TEMP",                "Ambient Air Temp"          , b"220046"    ,  0, coolant_temp, ECU.ALL    , False),  
+    'BAT_COOLANT_TEMP':                  OBDCommand("BAT_COOLANT_TEMP",                "Battery Coolant Temp"          , b"2241A4"    ,  0, coolant_temp, ECU.ALL    , False),  
     'ELEC_COOLANT_TEMP':                 OBDCommand("ELEC_COOLANT_TEMP",                "Electronics Coolant Temp"          , b"2241A4"    ,  0, coolant_temp, ECU.ALL    , False),  
     'AC_VOLTAGE':              OBDCommand("AC_VOLTAGE",                "AC Voltage"          , b"224368"    ,  0, ac_voltage, ECU.ALL    , False), 
     'AC_CURRENT':              OBDCommand("AC_CURRENT",                "AC Current"          , b"224369"    ,  0, ac_current, ECU.ALL    , False), 
